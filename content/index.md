356d720a7938167a2ca28712824fe585c67d4079
Nishanth Avva <avva.nishanth@gmail.com>
2024-01-27 23:59:23 +0100

    Quartz sync: Jan 27, 2024, 11:59 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,7 @@
 ---
-title: Welcome to Quartz
+title: Welcome to Nishanth's Digital Garden
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+# About
+
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Nishanth's Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
