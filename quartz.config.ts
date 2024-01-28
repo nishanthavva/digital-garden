73b4b797732b33c11b340bcc8c5b6741e1b63cf6
Nishanth Avva <avva.nishanth@gmail.com>
2024-01-28 08:09:51 +0100

    Quartz sync: Jan 28, 2024, 8:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nishanths-digital-garden.com/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
