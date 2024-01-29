f1086e9f3ea09d300eed4167b2ead072211ec3f9
Nishanth Avva <avva.nishanth@gmail.com>
2024-01-29 10:32:48 +0100

    Quartz sync: Jan 29, 2024, 10:32 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -47,7 +47,7 @@ const config: QuartzConfig = {
       Plugin.FrontMatter(),
       Plugin.TableOfContents(),
       Plugin.CreatedModifiedDate({
-        // you can add 'git' here for last modified from Git.
+        // you can add 'git' here for last modified from Git
         // if you do rely on git for dates, ensure defaultDateType is 'modified'
         priority: ["frontmatter", "filesystem"],
       }),
